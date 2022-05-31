51c48043b9cb761edf65257b4cf2c1c5935d7fa4
Mathieu Chartier <mathieuc@google.com>
2022-06-01 00:41:45 +0100

    universal7880-common: Compile system services using speed-profile

--- common.mk
+++ common.mk
@@ -327,6 +327,9 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhwbinder
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # System.prop
 TARGET_SYSTEM_PROP += device/samsung/universal7880-common/system.prop
 
